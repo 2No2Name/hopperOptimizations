0e87f734136d3e3e8c91505287b11a0396fd71af
gnembon <gnembonmc@gmail.com>
2019-06-27 23:43:06 -0300

    nothing important

--- src/main/java/carpet_extension/ExampleSettings.java
+++ src/main/java/carpet_extension/ExampleSettings.java
@@ -1,9 +1,10 @@
-package carpet_extension.mixins;
+package carpet_extension;
 
 import carpet.settings.ParsedRule;
 import carpet.settings.Rule;
 import carpet.settings.SettingsManager;
 import carpet.settings.Validator;
+import carpet.utils.Messenger;
 import net.minecraft.server.command.ServerCommandSource;
 
 import static carpet.settings.RuleCategory.CREATIVE;
@@ -15,11 +16,12 @@ public class ExampleSettings
         @Override
         public Integer validate(ServerCommandSource source, ParsedRule<Integer> currentRule, Integer newValue, String typedString)
         {
+            Messenger.m(source, "rb Congrats, you just changed a setting to "+newValue);
             return newValue < 20000000 ? newValue : null;
         }
     }
     @Rule(
-            desc = "Customizable fill/clone volume limit",
+            desc = "Example numerical setting",
             options = {"32768", "250000", "1000000"},
             validate = {Validator.POSITIVE_NUMBER.class, CheckFillLimitLimits.class},
             category = {CREATIVE, "skyblock"}
