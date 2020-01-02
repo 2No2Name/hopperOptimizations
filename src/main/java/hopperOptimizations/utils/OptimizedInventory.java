c562515ea5207c9c0ed5dae2711fb557bb273101
2No2Name <2No2Name@web.de>
2020-01-02 11:59:53 +0100

    Added Hoppers caching BlockInventories when OptimizedInventories is enabled even when OptimizedEntityHopperInteraction is not

--- src/main/java/hopperOptimizations/utils/OptimizedInventory.java
+++ src/main/java/hopperOptimizations/utils/OptimizedInventory.java
@@ -13,7 +13,7 @@ public interface OptimizedInventory extends Inventory {
     boolean mayHaveOptimizer(); //True when no player is looking into the inventory
 
 
-    //Only used for chests, cached double inventories can check whether they are still up to date.
+    //Only used for chests, cached double inventories can check whether they are still up to date. e.g. both chest halfs still exist
     default boolean isStillValid() {
         return true;
     }
