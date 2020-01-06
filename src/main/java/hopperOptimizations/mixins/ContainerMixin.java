d92b29b5db348630894f6eab3e730fd7c6943ef1
2No2Name <2No2Name@web.de>
2020-01-06 01:28:27 +0100

    Fixed player item transfer duplication caused by skipped stack.setCount in ContainerMixin

--- src/main/java/hopperOptimizations/mixins/ContainerMixin.java
+++ src/main/java/hopperOptimizations/mixins/ContainerMixin.java
@@ -30,58 +30,57 @@ public abstract class ContainerMixin {
     //Explicitly tell the Inventory when a stack is changed (setCount, incr, decr, splitStack), otherwise the optimizer
     //data will become outdated, which will lead to incorrect behavior (e.g. assuming all slots are occupied etc)
     //use InventoryOptimizer.DEBUG = true to check whether the data has become outdated due to unnoticed inventory changes.
-    private int tmpCount = 0;
+    private int newCount = 0;
+    private int prevCount = 0;
+    private int changeCount = 0;
+    private ItemStack curStack;
     private Inventory tmpInventory;
 
     @Redirect(method = "insertItem(Lnet/minecraft/item/ItemStack;IIZ)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setCount(I)V", ordinal = 0))
     private void helpNotifyInventory(ItemStack itemStack, int count) {
-        tmpCount = count;
+        prevCount = itemStack.getCount();
+        itemStack.setCount(count);
+        newCount = count;
+        curStack = itemStack;
     }
 
     @Inject(method = "insertItem(Lnet/minecraft/item/ItemStack;IIZ)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setCount(I)V", ordinal = 0, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
     private void notifyInventory(ItemStack stack, int startIndex, int endIndex, boolean fromLast, CallbackInfoReturnable<Boolean> cir, boolean bl, int i, Slot slot, ItemStack itemStack, int j) {
-        if (slot.inventory instanceof OptimizedInventory) {
+        if (Settings.optimizedInventories && slot.inventory instanceof OptimizedInventory) {
             InventoryOptimizer opt = ((OptimizedInventory) slot.inventory).getOptimizer();
             if (opt != null) {
                 int index = opt.indexOfObject(stack);
                 if (index == -1) {
                     //throw new IllegalStateException(); //this should not happen, but still handle it
                     ((OptimizedInventory) slot.inventory).invalidateOptimizer();
-                    stack.setCount(tmpCount);
                     return;
                 }
-                int prevStackSize = stack.getCount();
-                stack.setCount(tmpCount);
-                opt.onItemStackCountChanged(index, tmpCount - prevStackSize);
+                opt.onItemStackCountChanged(index, newCount - prevCount);
             }
-        } else {
-            stack.setCount(tmpCount);
         }
     }
 
     @Redirect(method = "insertItem(Lnet/minecraft/item/ItemStack;IIZ)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V"))
     private void helpNotifyInventory2(ItemStack itemStack, int count) {
-        tmpCount = count;
+        itemStack.decrement(count);
+        changeCount = -count;
+        curStack = itemStack;
     }
 
     @Inject(method = "insertItem(Lnet/minecraft/item/ItemStack;IIZ)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
     private void notifyInventory2(ItemStack stack, int startIndex, int endIndex, boolean fromLast, CallbackInfoReturnable<Boolean> cir, boolean bl, int i, Slot slot, ItemStack itemStack, ItemStack var10, int var11) {
-        if (slot.inventory instanceof OptimizedInventory) {
+        if (Settings.optimizedInventories && slot.inventory instanceof OptimizedInventory) {
             InventoryOptimizer opt = ((OptimizedInventory) slot.inventory).getOptimizer();
             if (opt != null) {
                 int index = opt.indexOfObject(stack);
                 if (index == -1) {
                     //throw new IllegalStateException(); //this should not happen, but still handle it
                     ((OptimizedInventory) slot.inventory).invalidateOptimizer();
-                    stack.decrement(tmpCount);
                     return;
                 }
-                stack.decrement(tmpCount);
-                opt.onItemStackCountChanged(index, -tmpCount);
-                return;
+                opt.onItemStackCountChanged(index, changeCount);
             }
         }
-        stack.decrement(tmpCount);
     }
 
     //Injecting at both ordinal 0 and 1
@@ -93,20 +92,20 @@ public abstract class ContainerMixin {
     //Redirecting at both ordinal 0 and 1
     @Redirect(method = "insertItem(Lnet/minecraft/item/ItemStack;IIZ)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;split(I)Lnet/minecraft/item/ItemStack;"))
     private ItemStack notifyInventory3(ItemStack itemStack, int count) {
-        if (tmpInventory instanceof OptimizedInventory) {
+        ItemStack ret = itemStack.split(count);
+        if (Settings.optimizedInventories && tmpInventory instanceof OptimizedInventory) {
             InventoryOptimizer opt = ((OptimizedInventory) tmpInventory).getOptimizer();
             if (opt != null) {
                 int index = opt.indexOfObject(itemStack);
                 if (index == -1) {
                     //throw new IllegalStateException(); //this should not happen, but still handle it
                     ((OptimizedInventory) tmpInventory).invalidateOptimizer();
-                    return itemStack.split(count);
+                    return ret;
                 }
-                ItemStack ret = itemStack.split(count);
                 opt.onItemStackCountChanged(index, -count);
                 return ret;
             }
         }
-        return itemStack.split(count);
+        return ret;
     }
 }
