9982bc80e489cc5c0b88e7ad0b947a382293552a
2No2Name <2No2Name@web.de>
2020-09-13 01:01:43 +0200

    change: remove lithium detection that crashed anyways

--- src/main/java/hopperOptimizations/settings/Settings.java
+++ src/main/java/hopperOptimizations/settings/Settings.java
@@ -4,16 +4,5 @@ package hopperOptimizations.settings;
 public class Settings {
 
     public static final boolean cacheInventories = true;
-    public static final boolean useEntityTrackerEngine;
-
-    static {
-        boolean present = true;
-        try {
-            Class.forName("me.jellysquid.mods.lithium.common.entity.tracker.nearby.ExactPositionListener");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Hopper Optimizations did not find Lithium with ExactPositionListeners. Disabling optimization!");
-            present = false;
-        }
-        useEntityTrackerEngine = present;
-    }
+    public static final boolean useEntityTrackerEngine = true;
 }
