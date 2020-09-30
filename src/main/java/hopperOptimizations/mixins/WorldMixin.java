ef7889c3f9a286bd657c90c54112b40cf69b99fa
2No2Name <2No2Name@web.de>
2020-09-30 19:26:16 +0200

    fix: unloading hoppers not deregistering from the entity tracker engine

---
+++ src/main/java/hopperOptimizations/mixins/WorldMixin.java
@@ -0,0 +1,30 @@
+package hopperOptimizations.mixins;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(World.class)
+public abstract class WorldMixin {
+    @Shadow
+    @Final
+    protected List<BlockEntity> unloadedBlockEntities;
+
+    @Inject(method = "tickBlockEntities",
+            at = @At(value = "INVOKE", target = "Ljava/util/List;removeAll(Ljava/util/Collection;)Z", ordinal = 0))
+    private void removeHoppersFromTrackerEngine(CallbackInfo ci) {
+        for (BlockEntity be : this.unloadedBlockEntities) {
+            if (be instanceof HopperBlockEntity) {
+                be.markRemoved();
+            }
+        }
+    }
+}
